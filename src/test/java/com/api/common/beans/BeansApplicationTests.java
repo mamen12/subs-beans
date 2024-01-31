67e8a586df3a809832f86f1dd48dc6a82013f6a2
Syahrul Ilham Muttaqien <syahrulilham62@gmail.com>
2024-02-01 01:41:16 +0700

    Initial Project

---
+++ src/test/java/com/api/common/beans/BeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.common.beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
